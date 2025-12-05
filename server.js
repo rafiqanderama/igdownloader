74f3e8a120674793bcf94027f5bc7268f03c2442
rafiqanderama <myfrangco@gmail.com>
2025-12-05 15:39:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -157,3 +157,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
+
+process.on("SIGINT", () => {
+  console.log("Received SIGINT, shutting down gracefully...");
+});
