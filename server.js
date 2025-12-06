9417e2fdd3c32a77a9964f5d37036819d5e12636
rafiqanderama <myfrangco@gmail.com>
2025-12-06 12:14:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const { execFile } = require("child_process");
+const COOKIE_DIR = "/app/cookies";
+const IG_COOKIE_PATH = `${COOKIE_DIR}/ig.txt`;
+const multer = require("multer");
 
 const app = express();
 app.use(cors());
@@ -58,6 +61,32 @@ app.post("/api/ig", async (req, res) => {
   } catch (e) {
     res.json({ error: e.toString() });
   }
+});
+
+if (!fs.existsSync(COOKIE_DIR)) fs.mkdirSync(COOKIE_DIR);
+
+// storage multer
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => cb(null, COOKIE_DIR),
+  filename: (_, __, cb) => cb(null, "ig.txt")
+});
+const upload = multer({ storage });
+
+// token admin
+const ADMIN_TOKEN = "goryto32";
+
+// middleware auth
+function checkAdmin(req, res, next) {
+  const token = req.headers["authorization"]?.replace("Bearer ", "");
+  if (token !== ADMIN_TOKEN)
+    return res.status(401).json({ error: "Unauthorized" });
+  next();
+}
+
+// endpoint upload cookie ig
+app.post("/admin/upload-ig-cookie", checkAdmin, upload.single("cookie"), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: "Tidak ada file" });
+  return res.json({ success: true, message: "IG cookie berhasil diupload!" });
 });
 
 app.listen(3000, () => console.log("IG Downloader running on port 3000"));
